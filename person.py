bacd956a1b449e327f26b521f7f056725c42b9b8
Aaron <113026288+AaronVanSchie@users.noreply.github.com>
2022-09-08 12:51:46 +0200

    Add files via upload

---
+++ person.py
@@ -0,0 +1,11 @@
+naam = input("Naam:")
+adres = input("Adres:")
+postcode = input("Postcode:")
+woonplaats = input("Woonplaats:")
+print(f'''
+--------------------
+| Naam: {naam}
+| Adres: {adres}
+| Postcode: {postcode}
+| Woonplaats: {woonplaats}
+--------------------''')

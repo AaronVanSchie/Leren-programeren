fdce6835968bd20e34e5cb949f0477180f2ab333
AaronVanSchie <99071407@mydavinci.nl>
2022-09-09 09:41:08 +0200

    hallo

---
+++ Module 2/conditions en forks/input en if-statement 1.py
@@ -0,0 +1,8 @@
+print("Een getal onder de 100?")
+a = int(input())
+print("Een getal onder de 100?")
+b = int(input())
+if b > a:
+    print("b is groter dan a")
+elif b < a:
+    print("b is kleiner dan a")

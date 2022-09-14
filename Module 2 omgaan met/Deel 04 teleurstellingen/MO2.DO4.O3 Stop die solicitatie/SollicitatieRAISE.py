35379f18a5236e1a3bb5c14762b4ce60bec4b4ff
AaronVanSchie <99071407@mydavinci.nl>
2022-09-14 13:56:53 +0200

    Hallo

---
+++ Module 2 omgaan met/Deel 04 teleurstellingen/MO2.DO4.O3 Stop die solicitatie/SollicitatieRAISE.py
@@ -0,0 +1,43 @@
+from multiprocessing.sharedctypes import Value
+
+
+print(f'''
+{"-" * 20}
+| Locatie : Rotterdam
+| Postcode : 5876 KA
+| Email : SolicitatieRotterdam@gmail.com
+{"-" * 20}''')
+naam = input("Wat is uw naam? ")
+if naam == "Dylan":
+    raise NameError("Ga we Dylan!!")
+dieren = int(input("Hoeveel jaar praktijkervaring heeft u met dieren-dressuur? "))
+jongleren = int(input("Hoeveel jaar ervaring heeft u met jongleren? "))
+acrobatiek = int(input("Hoeveel jaar praktijkervating heeft u met acrobatiek? "))
+diploma = input("Heeft u een MBO-4 diploma onderneming? J/N ") == "J"
+vrachtwagen = input("Bent u in bezit van een gelding vrachtwagen rijbewijs? J/N ") == "J"
+hoed = input("Heeft u een hoge hoed? J/N ") == "J"
+geslacht = input("Bent u een man of vrouw? M/V ")
+if geslacht == "M":
+   lsnor = int(input("Hoe breed is uw snor in cm? "))
+elif geslacht == "V":
+    roodhaar = input("Is uw haar rood? J/N ") == "J"
+    lhaar = int(input("Hoelang is uw haar in cm? "))
+lengte = int(input("Wat is uw lengte in cm? "))
+gewicht = int(input("Wat is uw lichaamsgewicht in kg? "))
+if gewicht >100:
+    raise ValueError("Ga sporten dikzak!")
+certificaat = input("Bent u in bezit van een cerificaat voor werken met gevaarlijk personeel? J/N ") == "J"
+hoogte = input("Heeft u hoogtevrees? J/N ") == "N"
+jett = input("Can jett revive jou? J/N ") == "J"
+douche = int(input("Hoevaak per week douched u? "))
+if douche <5:
+    raise ValueError("Ew ga meer douchen!!")
+discord = input("Gebruikt u discord? J/N ")
+if dieren >= 3 and jongleren >= 4 and acrobatiek >= 5 and diploma and vrachtwagen and hoed and (geslacht == "M" and lsnor >=10 or geslacht == "V" and roodhaar and lhaar >= 20) and lengte and gewicht and certificaat and hoogte and jett and douche >= 7 and discord :
+    print(f'''
+    Gefeliciteerd {naam}
+    u bent geschikt voor deze baan! ''')
+else:
+    print(f'''
+    Beste {naam} 
+    u bent helaas niet geschikt voor deze baan.''')

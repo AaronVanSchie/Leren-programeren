dd0b2607322e9a75aeade9af1ab2c7a43775c794
AaronVanSchie <99071407@mydavinci.nl>
2023-02-07 09:25:03 +0100

    Hello

--- Leerroutes/LeerenProgramere/M05 Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O4 - Namen en leeftijden/naamleeftijd.py
+++ Leerroutes/LeerenProgramere/M05 Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O4 - Namen en leeftijden/naamleeftijd.py
@@ -2,14 +2,15 @@ def name_age(i: list):
     name_input = input("Wat is de naam? ")
     leeftijd_input = input("Wat is de leeftijd? ")
     naam_leeftijd.append({
-    "Naam": name_input,
-    "Age": leeftijd_input
+        "Naam": name_input,
+        "Age": leeftijd_input
     })
+
 
 naam_leeftijd = []
 stoppen = False
 
-while stoppen == False:
+while stoppen != True:
     name_age(naam_leeftijd)
     stop = input("Wilt u stoppen? (In hoofdletters!!) J/N ")
     if stop == "J":

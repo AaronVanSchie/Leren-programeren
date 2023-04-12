38b942d45cc12f238626f85bdd7745b57b791d24
AaronVanSchie <99071407@mydavinci.nl>
2023-04-12 10:48:34 +0200

    hello

--- Leerroutes/Dynamische User Interfaces/M02 DOM Manipulate & Event Handeling/M02.O4 - Buttons 3.0/buttons.js
+++ Leerroutes/Dynamische User Interfaces/M02 DOM Manipulate & Event Handeling/M02.O4 - Buttons 3.0/buttons.js
@@ -1,17 +1,18 @@
-const kleuren = ["red","purple","blue","black"];
-let index = 0
+const kleuren = ["green", "red","purple","blue","black"];
+
+function CheckColour(button){
+    buttonkleur = button.style.backgroundColor
+    nr=kleuren.indexOf(buttonkleur)
+    document.getElementById(button).style.backgroundColor = kleuren[nr+1]
+    if ((nr+1)==kleuren.length-1){
+        document.getElementById(button).remove()
+    }
+}
 
 for(let i = 1; i< 30 + 1; i++){
-    const div = document.createElement('button');
+    const div = document.createElement("button");
     div.textContent = i
-
-div.addEventListener('click', function Onclick() {
-    div.style.backgroundColor = kleuren[index];
-    if (index in kleuren){
-        index = index + 1
-    } else if (!(index in kleuren)) {
-        index = index - 4
-    }
-  });
+    div.style.backgroundColor = kleuren[0];
+    div.setAttribute("onClick", "CheckColour(this)");
     container.appendChild(div)
 }

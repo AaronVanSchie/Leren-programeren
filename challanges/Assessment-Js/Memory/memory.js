d6364d2fd99826a46fad20a85812e6859d63fb34
AaronVanSchie <99071407@mydavinci.nl>
2023-06-08 10:19:07 +0200

    memory

--- challanges/Assessment-Js/Memory/memory.js
+++ challanges/Assessment-Js/Memory/memory.js
@@ -10,21 +10,4 @@ const cards = ["Cola", "Sprite", "Fanta", "Dr Pepper", "7uP", "Red Bull", "Wiske
     }
 
     function flipCard(card) {
-        if (!card.classList.contains('flipped')) {
-            card.classList.add('flipped');
-            flippedCards.push(card);
-
-            if (flippedCards.length === 2) {
-                setTimeout(() => {
-                    const [card1, card2] = flippedCards;
-                    if (card1.textContent === card2.textContent) {
-                        flippedCards = [];
-                    } else {
-                        card1.classList.remove('flipped');
-                        card2.classList.remove('flipped');
-                        flippedCards = [];
-                    }
-                }, 1000);
-            }
-        }
     }

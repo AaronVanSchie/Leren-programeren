6c406068448972095e456f44df5239caad08d8fa
AaronVanSchie <99071407@mydavinci.nl>
2023-06-08 12:15:06 +0200

    memory

--- challanges/Assessment-Js/Memory/memory.js
+++ challanges/Assessment-Js/Memory/memory.js
@@ -1,13 +1,48 @@
-const cards = ["Cola", "Sprite", "Fanta", "Dr Pepper", "7uP", "Red Bull", "Wiskey", "AA", "Casis", "Water"];
-    let flippedCards = [];
+const images = [
+    'images/bacardi.jpg',
+    'images/malibu.jpg',
+    'images/vodka.jpg',
+    'images/passoa.jpg',
+    'images/wiskey.jpg',
+];
 
-    function createCard(card) {
-        const element = document.createElement('div');
-        element.textContent = card;
-        element.classList.add('card');
-        element.addEventListener('click',  flipCard(element));
-        return element;
+const kaarten = images.concat(images);
+
+let score = 0;
+let flippedkaartjes = [];
+
+const scoreDisplay = document.getElementById('score');
+const kaartjes = document.getElementById('images');
+
+function shuffle(kaartje) {
+    let IndexNu = kaartje.length;
+    let Value, randomIndex;
+
+    while (IndexNu !== 0) {
+        randomIndex = Math.floor(Math.random() * IndexNu);
+        IndexNu -= 1;
+        Value = kaartje[IndexNu];
+        kaartje[IndexNu] = kaartje[randomIndex];
+        kaartje[randomIndex] = Value;
     }
 
-    function flipCard(card) {
+    return kaartje;
+}
+
+function createBoard() {
+    const shuffeld = shuffle(kaarten);
+    for (let i = 0; i < shuffeld.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.cardIndex = i;
+
+        const kaart = document.createElement('img');
+        kaart.src = shuffeld[i];
+        kaart.classList.add('hidden');
+
+        card.appendChild(kaart);
+        Board.appendChild(card);
+
+        card.addEventListener('click', flipCard);
     }
+}

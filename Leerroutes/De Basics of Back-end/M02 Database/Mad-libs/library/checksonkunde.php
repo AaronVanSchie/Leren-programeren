043f1eaf087589d16396e456cde97c739191f00c
AaronVanSchie <99071407@mydavinci.nl>
2023-06-13 10:21:24 +0200

    update

---
+++ Leerroutes/De Basics of Back-end/M02 Database/Mad-libs/library/checksonkunde.php
@@ -0,0 +1,59 @@
+<?php
+$kunnenErr = $opschietenErr = $getalErr = $vakantieErr = $goedErr = $slechtErr = $overkomenErr = "";
+$kunnen = $opschieten = $getal = $vakantie = $goed = $slecht = $overkomen = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["kunnen"])) {
+        $kunnenErr = "Dit vak is verplicht";
+      } else {
+        $kunnen = test_input($_POST["kunnen"]);
+      }
+    
+    if (empty($_POST["opschieten"])) {
+        $opschietenErr = "Dit vak is verplicht";
+        } else {
+            $opschieten = test_input($_POST["opschieten"]);
+        }
+
+    if (empty($_POST["getal"])) {
+        $getalErr = "Dit vak is verplicht";
+        } else {
+            $getal = test_input($_POST["getal"]);
+        }
+
+    if (empty($_POST["vakantie"])) {
+        $vakantieErr = "Dit vak is verplicht";
+        } else {
+            $vakantie = test_input($_POST["vakantie"]);
+        }
+
+    if (empty($_POST["goed"])) {
+        $goedErr = "Dit vak is verplicht";
+        } else {
+            $goed = test_input($_POST["goed"]);
+        }
+    if (empty($_POST["slecht"])) {
+        $slechtErr = "Dit vak is verplicht";
+        } else {
+            $slecht = test_input($_POST["slecht"]);
+        }
+    if (empty($_POST["overkomen"])) {
+        $overkomenErr = "Dit vak is verplicht";
+        } else {
+            $overkomen = test_input($_POST["overkomen"]);
+        }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+<<<<<<< HEAD
+$everything1 = $kunnen && $opschieten && $getal && $vakantie && $goed && $slecht && $overkomen;
+=======
+$everything = $kunnen && $opschieten && $getal && $vakantie && $goed && $slecht && $overkomen;
+>>>>>>> 03c70c075dc97ed285a59b6551469dd70dce0d3d
---
+++ Leerroutes/De Basics of Back-end/M02 Database/Mad-libs/needed/buttons.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <html lang="nl">
+    <title>Keuzes</title>
+    <link rel="stylesheet" href="needed/style.css">
+</head>
+    <body>
+        <div class = "items">
+            <a href="index.php">Index</a> |
+            <a href="onkunde.php">Onkunde</a> |
+            <a href="paniek.php">Paniek</a>
+        </div>
+</html>
